204956369262ae46df101c12256d54db48fda150
plotnips <pavel.plotnitskiy@gmail.com>
2024-09-17 09:41:36 +0300

    update

--- test_rtm_munich_perf.sh
+++ test_rtm_munich_perf.sh
@@ -3,16 +3,13 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=pavel.plotnitskii@kaust.edu.sa
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --threads-per-core=1
-#SBATCH --mem=50GB
-#SBATCH --time=24:00:00
+#SBATCH --nodelist=gbt04
+#SBATCH --time=72:00:00
 #SBATCH --partition=7773X  # Milan-X 128
-#SBATCH --job-name=test_rtm
-#SBATCH --output=logs/rtm.%J.out
-#SBATCH --error=logs/rtm.%J.err
-#SBATCH --cpus-per-task=128
-#SBATCH --hint=nomultithread    # don't use hyperthreading
+#SBATCH --job-name=rtm
+#SBATCH --output=./logs/rtm.%J.out
+#SBATCH --error=./logs/rtm.%J.err
+#SBATCH --hint=nomultithread
 
 ###******** HORODATED LOG WRITING *********###
 exec > >(while read line; do echo "$(date): $line"; done | tee log-rtm.log) 2>&1
