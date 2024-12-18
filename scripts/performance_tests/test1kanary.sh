8ed1f8bef2d98f6014944908654a561239914b84
plotnips <pavel.plotnitskiy@gmail.com>
2024-12-18 20:34:34 +0300

    modify test

--- scripts/performance_tests/test1kanary.sh
+++ scripts/performance_tests/test1kanary.sh
@@ -98,8 +98,7 @@ for i in $(seq 0 1); do
   echo "Running TB"
   echo "Running 1st order"
   echo "num_th=${OMP_NUM_THREADS}, th_x=${th_x}, th_y=${th_y}, th_z=${th_z}, num_wf=${num_wf}, t_dim=${t_dim}"
-  numactl --interleave=all \
-  ./bin/modeling --verbose --n1 $nx --n2 $ny --n3 $nz --iter $NT_TB_1st --tb_thread_group_size $tgs \
+  numactl --interleave=all ./bin/modeling --verbose --n1 $nx --n2 $ny --n3 $nz --iter $NT_TB_1st --tb_thread_group_size $tgs \
   --tb_nb_thread_groups $(expr $OMP_NUM_THREADS / $tgs) --tb_th_x $th_x --tb_th_y $th_y --tb_th_z $th_z \
   --tb_t_dim $t_dim --tb_num_wf $num_wf --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot -c \
   --src_depth $src_depth --order 1 --fmax $fmax --dx $dx >> $logs_path/log-TB_1st-abc_$grid_str.log;
