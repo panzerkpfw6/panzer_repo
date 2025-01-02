63e8b5db2caf88c113c986e094e0ecc5e15ed825
plotnips <pavel.plotnitskiy@gmail.com>
2025-01-02 18:01:22 +0300

    modify test

--- scripts/j_compare_wavefields_for_SB_TB.sh
+++ scripts/j_compare_wavefields_for_SB_TB.sh
@@ -19,28 +19,15 @@ mv -f ./CMakeCache.txt ./CMakeCache-old.txt    #Last CMakeCache.txt is saved
 ##### build the project with debugging symbols
 #CC=icc CXX=icpc -DCMAKE_BUILD_TYPE=Debug cmake .
 #CC=icc CXX=icpc -O0 -g3 -DCMAKE_BUILD_TYPE=Debug cmake .
+
 ##### build the project
-#CC=icc CXX=icpc cmake .
+CC=icc CXX=icpc cmake .
 #CC=icc CXX=icpc cmake -DCMAKE_C_FLAGS="-pg" -DCMAKE_CXX_FLAGS="-pg" .
-CC=icc CXX=icpc cmake -DCMAKE_C_FLAGS="-pg -O2" -DCMAKE_CXX_FLAGS="-pg -O2" .
+#CC=icc CXX=icpc cmake -DCMAKE_C_FLAGS="-pg -O2" -DCMAKE_CXX_FLAGS="-pg -O2" .
 
 make clean
 make VERBOSE=1
 make install
-############################### compare_wavefields_for_SB_TB_1st_order
-#nx=128;ny=256;nz=512;
-#dt=0.001;
-#shot=16447;  # position of the source in x,y coordinates.check ./data/acquisition.txt
-#src_depth=256;
-#x=2; y=2; z=1; t=7; w=20; tgs=4;
-#export OMP_NUM_THREADS=4
-
-#./bin/modeling --verbose --n1 $nx  --n2 $ny --n3 $nz --iter $TIME_SB_1st \
-#--mode 2 --drcv 1 --dshot 1 --first $shot --last $shot --src_depth 256 --order 1 --fmax 8;
-#./bin/modeling --verbose --n1 $nx  --n2 $ny --n3 $nz --iter $TIME_TB_1st --tb_thread_group_size $tgs \
-# --tb_nb_thread_groups $(expr $OMP_NUM_THREADS / $tgs) --tb_th_x $x --tb_th_y $y --tb_th_z $z \
-# --tb_t_dim $t --tb_num_wf $w --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot -c --src_depth 256 --order 1 --fmax 8;
-# ./scripts_useful/diff_to ./snapshot_TB1st_512 ./snapshot_SB1st_511
 
 ############################### parameters
 nx=256;ny=256;nz=256;
@@ -88,18 +75,6 @@ export OMP_NUM_THREADS=12;
 #  --tb_nb_thread_groups $(expr $OMP_NUM_THREADS / $tgs) --tb_th_x $x --tb_th_y $y --tb_th_z $z \
 #  --tb_t_dim $t --tb_num_wf $w --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot -c --src_depth $src_depth --order 1 --fmax 8 --dx 10;
 
-### debugging with gprof
-#gprof my_program gmon.out > analysis.txt
-
-#gprof ./bin/modeling --verbose --n1 $nx  --n2 $ny --n3 $nz --iter $TIME_SB_1st \
-# --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot --src_depth $src_depth --order 1 --fmax 8 --dx 10  gmon.out > analysis.txt;
-
-rm gmon.out
-./bin/modeling --verbose --n1 $nx  --n2 $ny --n3 $nz --iter $TIME_SB_1st \
-  --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot --src_depth $src_depth --order 1 --fmax 8 --dx 10  gmon.out > analysis.txt;
-gprof ./bin/modeling gmon.out > analysis.txt
-
-
 #gdb --args ./bin/modeling --verbose --n1 $nx --n2 $ny --n3 $nz --iter $TIME_TB_1st --tb_thread_group_size $tgs \
 #  --tb_nb_thread_groups $(expr $OMP_NUM_THREADS / $tgs) --tb_th_x $x --tb_th_y $y --tb_th_z $z \
 #  --tb_t_dim $t --tb_num_wf $w --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot -c --src_depth $src_depth --order 1 --fmax 8 --dx 10;
@@ -107,8 +82,3 @@ gprof ./bin/modeling gmon.out > analysis.txt
 #./scripts_useful/diff_to ./snapshot_TB1st_537 ./snapshot_SB1st_537;
 
 ############################################################
-############################################################
-#./bin/modeling --verbose --n1 $nx  --n2 $ny --n3 $nz --iter $TIME_SB_1st \
-# --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot --src_depth $src_depth --order 1 --fmax 8 --dx 10 /
-#  >> aa.log;
-
