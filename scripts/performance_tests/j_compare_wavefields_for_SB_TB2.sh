3e59e4bb04314d4abb4696b99d18b97aba5426a0
plotnips <pavel.plotnitskiy@gmail.com>
2025-02-20 14:02:17 +0300

    modify test

--- scripts/performance_tests/j_compare_wavefields_for_SB_TB2.sh
+++ scripts/performance_tests/j_compare_wavefields_for_SB_TB2.sh
@@ -66,13 +66,13 @@ make VERBOSE=1
 make install
 
 #####################
-#echo "Running SB"
-#echo "Running 1st order"
-#./bin/modeling --verbose --n1 $nx --n2 $ny --n3 $nz --iter $NT_SB_1st \
-#--mode 2 --drcv 1 --dshot 1 --first $shot --last $shot --src_depth $src_depth --order 1 --fmax $fmax \
-#--dx $dx
-## >> $logs_path/log-SB_1st-abc_$grid_str.log
-### After execution, run gprof to generate the profiling report
+echo "Running SB"
+echo "Running 1st order"
+./bin/modeling --verbose --n1 $nx --n2 $ny --n3 $nz --iter $NT_SB_1st \
+--mode 2 --drcv 1 --dshot 1 --first $shot --last $shot --src_depth $src_depth --order 1 --fmax $fmax \
+--dx $dx
+# >> $logs_path/log-SB_1st-abc_$grid_str.log
+## After execution, run gprof to generate the profiling report
 #gprof ./bin/modeling gmon.out > $logs_path/gprof_report.txt
 
 #####################
@@ -83,8 +83,8 @@ make install
 #perf report > $logs_path/perf_report.txt
 
 #####################
-echo "Running memory check with valgrind..."
-valgrind --tool=memcheck --leak-check=full --show-reachable=yes --track-origins=yes \
-./bin/modeling --verbose --n1 $nx --n2 $ny --n3 $nz --iter $NT_SB_1st \
---mode 2 --drcv 1 --dshot 1 --first $shot --last $shot --src_depth $src_depth --order 1 --fmax $fmax \
---dx $dx 2> $logs_path/valgrind_memcheck.txt
+#echo "Running memory check with valgrind..."
+#valgrind --tool=memcheck --leak-check=full --show-reachable=yes --track-origins=yes \
+#./bin/modeling --verbose --n1 $nx --n2 $ny --n3 $nz --iter $NT_SB_1st \
+#--mode 2 --drcv 1 --dshot 1 --first $shot --last $shot --src_depth $src_depth --order 1 --fmax $fmax \
+#--dx $dx 2> $logs_path/valgrind_memcheck.txt
