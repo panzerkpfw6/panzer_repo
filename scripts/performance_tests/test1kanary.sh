a092e8c54d84cf8698ac6c12f675697c498a8bbf
plotnips <pavel.plotnitskiy@gmail.com>
2025-03-23 02:22:00 +0300

    modify test

--- scripts/performance_tests/test1kanary.sh
+++ scripts/performance_tests/test1kanary.sh
@@ -69,8 +69,8 @@ export dx=10;
 
 ##### Run tests #####
 len=${#nx_arr[@]}
-#for i in $(seq 0 $len); do
-for i in $(seq 2 3); do
+for i in $(seq 0 $len); do
+#for i in $(seq 2 3); do
   echo $i
   nx=${nx_arr[$i]}
   ny=${ny_arr[$i]}
@@ -99,13 +99,13 @@ for i in $(seq 2 3); do
   echo "Running TB"
   echo "Running 1st order"
   echo "num_th=${OMP_NUM_THREADS}, th_x=${th_x}, th_y=${th_y}, th_z=${th_z}, num_wf=${num_wf}, t_dim=${t_dim}"
-  ./bin/modeling --verbose --n1 $nx --n2 $ny --n3 $nz --iter $NT_TB_1st --tb_thread_group_size $tgs \
+  numactl --interleave=all ./bin/modeling --verbose --n1 $nx --n2 $ny --n3 $nz --iter $NT_TB_1st --tb_thread_group_size $tgs \
   --tb_nb_thread_groups $(expr $OMP_NUM_THREADS / $tgs) --tb_th_x $th_x --tb_th_y $th_y --tb_th_z $th_z \
   --tb_t_dim $t_dim --tb_num_wf $num_wf --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot -c \
   --src_depth $src_depth --order 1 --fmax $fmax --dx $dx >> $logs_path/log-TB_1st-abc_$grid_str.log;
 
   echo "Running 2nd order"
-  ./bin/modeling --verbose --n1 $nx --n2 $ny --n3 $nz --iter $NT_TB_2nd --tb_thread_group_size $tgs \
+  numactl --interleave=all ./bin/modeling --verbose --n1 $nx --n2 $ny --n3 $nz --iter $NT_TB_2nd --tb_thread_group_size $tgs \
   --tb_nb_thread_groups $(expr $OMP_NUM_THREADS / $tgs) --tb_th_x $th_x --tb_th_y $th_y --tb_th_z $th_z \
   --tb_t_dim $t_dim --tb_num_wf $num_wf --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot -c \
   --src_depth $src_depth --order 2 --fmax $fmax --dx $dx >> $logs_path/log-TB_2nd-abc_$grid_str.log;
