3a9d6edef31b21d3728a1e893e6e99fc62890d11
plotnips <pavel.plotnitskiy@gmail.com>
2025-03-23 02:58:18 +0300

    modify test

--- scripts/performance_tests/test1kanary.sh
+++ scripts/performance_tests/test1kanary.sh
@@ -36,7 +36,7 @@ export _CB_SIZE_Y=4;
 th_x_arr=(4 4 4)
 th_y_arr=(2 2 2)
 th_z_arr=(2 1 1)
-num_wf_arr=(64 4 4)
+num_wf_arr=(64 64 64)
 tdim_arr=(7 7  7)
 
 ###*********** Experiment setup ************###
@@ -74,7 +74,7 @@ export dx=10;
 
 ##### Run tests #####
 len=${#nx_arr[@]}
-for i in $(seq 0 $len); do
+for i in $(seq 1 $len); do
 #for i in $(seq 2 3); do
   echo $i
   nx=${nx_arr[$i]}
@@ -90,28 +90,28 @@ for i in $(seq 0 $len); do
   tgs=$((th_x * th_y*th_z))
   ###*********** SB ************###
   cbx=16; cby=4; cbz=9999;
-  echo "Running SB"
-  echo "Running 1st order"
-  ./bin/modeling --verbose --n1 $nx  --n2 $ny --n3 $nz --iter $NT_SB_1st \
-  --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot --src_depth $src_depth --order 1 --fmax $fmax \
-  --dx $dx --cbx $cbx --cby $cby --cbz $cbz >> $logs_path/log-SB_1st-abc_$grid_str.log
-
-  echo "Running 2nd order"
-  ./bin/modeling --verbose --n1 $nx  --n2 $ny --n3 $nz --iter $NT_SB_2nd \
-  --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot --src_depth $src_depth --order 2 --fmax $fmax \
-  --dx $dx --cbx $cbx --cby $cby --cbz $cbz >> $logs_path/log-SB_2nd-abc_$grid_str.log
+#  echo "Running SB"
+#  echo "Running 1st order"
+#  ./bin/modeling --verbose --n1 $nx  --n2 $ny --n3 $nz --iter $NT_SB_1st \
+#  --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot --src_depth $src_depth --order 1 --fmax $fmax \
+#  --dx $dx --cbx $cbx --cby $cby --cbz $cbz >> $logs_path/log-SB_1st-abc_$grid_str.log
+#
+#  echo "Running 2nd order"
+#  ./bin/modeling --verbose --n1 $nx  --n2 $ny --n3 $nz --iter $NT_SB_2nd \
+#  --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot --src_depth $src_depth --order 2 --fmax $fmax \
+#  --dx $dx --cbx $cbx --cby $cby --cbz $cbz >> $logs_path/log-SB_2nd-abc_$grid_str.log
   ###*********** TB ************##
   echo "Running TB"
   echo "Running 1st order"
   echo "num_th=${OMP_NUM_THREADS}, th_x=${th_x}, th_y=${th_y}, th_z=${th_z}, num_wf=${num_wf}, t_dim=${t_dim}"
-  ./bin/modeling --verbose --n1 $nx --n2 $ny --n3 $nz --iter $NT_TB_1st --tb_thread_group_size $tgs \
+  numactl --interleave=all ./bin/modeling --verbose --n1 $nx --n2 $ny --n3 $nz --iter $NT_TB_1st --tb_thread_group_size $tgs \
   --tb_nb_thread_groups $(expr $OMP_NUM_THREADS / $tgs) --tb_th_x $th_x --tb_th_y $th_y --tb_th_z $th_z \
   --tb_t_dim $t_dim --tb_num_wf $num_wf --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot -c \
   --src_depth $src_depth --order 1 --fmax $fmax --dx $dx >> $logs_path/log-TB_1st-abc_$grid_str.log;
 
-  echo "Running 2nd order"
-  ./bin/modeling --verbose --n1 $nx --n2 $ny --n3 $nz --iter $NT_TB_2nd --tb_thread_group_size $tgs \
-  --tb_nb_thread_groups $(expr $OMP_NUM_THREADS / $tgs) --tb_th_x $th_x --tb_th_y $th_y --tb_th_z $th_z \
-  --tb_t_dim $t_dim --tb_num_wf $num_wf --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot -c \
-  --src_depth $src_depth --order 2 --fmax $fmax --dx $dx >> $logs_path/log-TB_2nd-abc_$grid_str.log;
+#  echo "Running 2nd order"
+#  numactl --interleave=all ./bin/modeling --verbose --n1 $nx --n2 $ny --n3 $nz --iter $NT_TB_2nd --tb_thread_group_size $tgs \
+#  --tb_nb_thread_groups $(expr $OMP_NUM_THREADS / $tgs) --tb_th_x $th_x --tb_th_y $th_y --tb_th_z $th_z \
+#  --tb_t_dim $t_dim --tb_num_wf $num_wf --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot -c \
+#  --src_depth $src_depth --order 2 --fmax $fmax --dx $dx >> $logs_path/log-TB_2nd-abc_$grid_str.log;
 done
