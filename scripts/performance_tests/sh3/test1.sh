01d8c04213ce18bae29464f98f2537296316276b
plotnips <pavel.plotnitskiy@gmail.com>
2025-03-26 21:45:52 +0300

    modify test

--- scripts/performance_tests/sh3/test1.sh
+++ scripts/performance_tests/sh3/test1.sh
@@ -133,7 +133,8 @@ for i in $(seq 0 $len); do
   num_wf=${num_wf_arr_1st[$i]}
   tgs=$((th_x * th_y*th_z))
   echo "num_th=${OMP_NUM_THREADS},th_x=${th_x},th_y=${th_y},th_z=${th_z},num_wf=${num_wf},t_dim=${t_dim},tgs=${tgs}"
-  srun --nodes=1 --cpus-per-task=$OMP_NUM_THREADS --hint=nomultithread --unbuffered numactl --interleave=all \
+
+  srun --nodes=1 --cpus-per-task=$OMP_NUM_THREADS --hint=nomultithread --threads-per-core=1 --unbuffered numactl --interleave=all \
   ./bin/modeling --verbose --n1 $nx --n2 $ny --n3 $nz --iter $NT_TB_1st --tb_thread_group_size $tgs \
   --tb_nb_thread_groups $(expr $OMP_NUM_THREADS / $tgs) --tb_th_x $th_x --tb_th_y $th_y --tb_th_z $th_z \
   --tb_t_dim $t_dim --tb_num_wf $num_wf --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot -c \
@@ -146,7 +147,7 @@ for i in $(seq 0 $len); do
   t_dim=${tdim_arr_2nd[$i]}
   num_wf=${num_wf_arr_2nd[$i]}
   tgs=$((th_x*th_y*th_z))
-  srun --nodes=1 --cpus-per-task=$OMP_NUM_THREADS --hint=nomultithread --unbuffered numactl --interleave=all \
+  srun --nodes=1 --cpus-per-task=$OMP_NUM_THREADS --hint=nomultithread --threads-per-core=1 --unbuffered numactl --interleave=all \
   ./bin/modeling --verbose --n1 $nx --n2 $ny --n3 $nz --iter $NT_TB_2nd --tb_thread_group_size $tgs \
   --tb_nb_thread_groups $(expr $OMP_NUM_THREADS / $tgs) --tb_th_x $th_x --tb_th_y $th_y --tb_th_z $th_z \
   --tb_t_dim $t_dim --tb_num_wf $num_wf --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot -c \
