c455c4499eba38214e2f8df4b31385fd6ad1e957
plotnips <pavel.plotnitskiy@gmail.com>
2025-04-08 14:31:47 +0300

    add python direct wave removal

---
+++ scripts_useful/python/remove_direct_wave.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import os,sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+import re
+import math
+#######################################
+
+
+
+
+
+
+
+
