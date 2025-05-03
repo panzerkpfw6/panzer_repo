0b9d100ea0d6f9b1489c86ea7f0d80961460f0ec
plotnips <pavel.plotnitskiy@gmail.com>
2025-05-03 22:22:59 +0300

    modify test

--- jenkins_rtm_shaheen.sh
+++ jenkins_rtm_shaheen.sh
@@ -53,7 +53,7 @@ CC=icc CXX=icpc cmake .
 make clean
 make VERBOSE=1
 make install
-
+export logs_path=./logs/rtm
 ################################
 echo "Test 1. compare_rtm_result_for_SB_TB"
 #@pavel in TB source injection starts from second time sample (Nothing happens for one dt).This is code feature.
