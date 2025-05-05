51b97f2bd50ea9b8483d920b260f91e38df86ae3
plotnips <pavel.plotnitskiy@gmail.com>
2025-05-05 20:36:19 +0300

    modify test

--- scripts/performance_tests/sh3/rtm_salt3d_tb.sh
+++ scripts/performance_tests/sh3/rtm_salt3d_tb.sh
@@ -89,6 +89,10 @@ first=1;last=100;
 dshot=4568;
 fmax=11;
 cbx=64;cby=22;cbz=9999;
+##################  TB parameters  ###################
+x=3; y=2; z=2; t=3; w=24; 
+tgs=x*y*z; echo $tgs;
+######################################################
 
 #./bin/modeling --verbose --n1 $nx --n2 $ny --n3 $nz --iter $timesteps --dshot $dshot --mode 2 \
 # --first $first --last $last --fwd_steps 3 --order 1 --fmax $fmax --src_depth 5 --rcv_depth 8 --drcv 1 \
@@ -108,21 +112,21 @@ echo "Model data for RTM. salt3d."
 srun --nodes=1 --cpus-per-task=$OMP_NUM_THREADS --hint=nomultithread --threads-per-core=1 \
 ./bin/modeling --verbose --n1 $nx --n2 $ny --n3 $nz --iter $timesteps --dshot $dshot --mode 2 \
 --first $first --last $last --fwd_steps 3 --order 1 --fmax $fmax --src_depth 5 --rcv_depth 8 --drcv 1 \
---dx $dh --dy $dh --dz $dh --dt $dt  \
---cbx $cbx --cby $cby --cbz $cbz  >> $logs_path/log_model_salt3d.log;
+--tb_thread_group_size $tgs --tb_nb_thread_groups $(expr $OMP_NUM_THREADS / $tgs) --tb_th_x $x --tb_th_y $y --tb_th_z $z \
+--tb_t_dim $t --tb_num_wf $w --dx $dh --dy $dh --dz $dh --dt $dt >> $logs_path/log_model_salt3d.log;
 
 echo "Perform RTM"
 srun --nodes=1 --cpus-per-task=$OMP_NUM_THREADS --hint=nomultithread --threads-per-core=1 \
 ./bin/rtm --verbose --n1 $nx --n2 $ny --n3 $nz --iter $timesteps --dshot $dshot --mode 2 \
 --first $first --last $last --fwd_steps 3 --order 1 --fmax $fmax --src_depth 5 --rcv_depth 8 --drcv 1 \
---dx $dh --dy $dh --dz $dh --dt $dt \
---cbx $cbx --cby $cby --cbz $cbz >> $logs_path/log_rtm_salt3d.log;
-
+--tb_thread_group_size $tgs --tb_nb_thread_groups $(expr $OMP_NUM_THREADS / $tgs) --tb_th_x $x --tb_th_y $y --tb_th_z $z \
+--tb_t_dim $t --tb_num_wf $w --dx $dh --dy $dh --dz $dh --dt $dt >> $logs_path/log_rtm_salt3d.log;
 
 echo "Gather images"
 srun --nodes=1 --cpus-per-task=$OMP_NUM_THREADS --hint=nomultithread --threads-per-core=1 \
 ./bin/gather --verbose --n1 $nx --n2 $ny --n3 $nz --iter $timesteps --dshot $dshot --mode 2 \
- --first $first --last $last -c --fwd_steps 3 --order 1 --src_depth 5 --rcv_depth 8 --drcv 1 --dir "./data" \
---cbx $cbx --cby $cby --cbz $cbz  >> $logs_path/log_gather_salt3d.log;
+--first $first --last $last -c --fwd_steps 3 --order 1 --src_depth 5 --rcv_depth 8 --drcv 1 --dir "./data" \
+--tb_thread_group_size $tgs --tb_nb_thread_groups $(expr $OMP_NUM_THREADS / $tgs) --tb_th_x $x --tb_th_y $y --tb_th_z $z \
+--tb_t_dim $t --tb_num_wf $w --dx $dh --dy $dh --dz $dh --dt $dt >> $logs_path/log_gather_salt3d.log;
 
 
