739e4c0c1d63a5697df122c2af679a452377d801
plotnips <pavel.plotnitskiy@gmail.com>
2025-05-07 22:35:41 +0300

    modify test

--- scripts/performance_tests/sh3/test1_modeling.sh
+++ scripts/performance_tests/sh3/test1_modeling.sh
@@ -120,10 +120,7 @@ for i in $(seq 0 2); do
   ###*********** SB ************###
   echo "Running SB"
   echo "Running 1st order"
-  srun --nodes=1 --cpus-per-task=$OMP_NUM_THREADS --hint=nomultithread --threads-per-core=1 \
-  ./bin/modeling --verbose --n1 $nx  --n2 $ny --n3 $nz --iter $NT_SB_1st \
-  --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot --src_depth $src_depth --order 1 --fmax $fmax \
-  --dx $dx --cbx $cbx --cby $cby --cbz $cbz >> $logs_path/test1_fwdSB.log;
+  srun --nodes=1 --cpus-per-task=$OMP_NUM_THREADS --hint=nomultithread --threads-per-core=1 ./bin/modeling --verbose --n1 $nx  --n2 $ny --n3 $nz --iter $NT_SB_1st --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot --src_depth $src_depth --order 1 --fmax $fmax --cbx $cbx --cby $cby --cbz $cbz >> $logs_path/test1_fwdSB.log;
 
   ###*********** TB ************##
 #  echo "Running TB"
