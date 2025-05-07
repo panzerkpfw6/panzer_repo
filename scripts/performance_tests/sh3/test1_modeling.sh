237d20e7e4d44e141a44d316f2e6dd3d4264c601
plotnips <pavel.plotnitskiy@gmail.com>
2025-05-07 19:34:34 +0300

    modify test

--- scripts/performance_tests/sh3/test1_modeling.sh
+++ scripts/performance_tests/sh3/test1_modeling.sh
@@ -64,11 +64,23 @@ export fmax=8;
 export dx=10;
 
 ###********** Default SB, TB parameters *********###
-th_x_arr_1st=(3 4 4)
+cbx_arr=(  8  16  22  )
+cby_arr=(  6  6  46  )
+cbz_arr=(  9999  9999  9999   )
+
+## My recent parameter search
+th_x_arr_1st=(16 16 16)
 th_y_arr_1st=(2 2 2)
-th_z_arr_1st=(2 1 1)
-tdim_arr_1st=(3 3 7)
-num_wf_arr_1st=(24 20 4)
+th_z_arr_1st=(1 1 1)
+tdim_arr_1st=(7 7 7)
+num_wf_arr_1st=(192 192 192)
+
+## PASC paper results
+#th_x_arr_1st=(16 4 4)
+#th_y_arr_1st=(2 2 2)
+#th_z_arr_1st=(1 1 1)
+#tdim_arr_1st=(7 3 7)
+#num_wf_arr_1st=(192 20 4)
 
 ###*********** Experiment setup ************###
 nx_arr=(  512  1024  2048  )
@@ -100,17 +112,19 @@ for i in $(seq 0 $len); do
   nx=${nx_arr[$i]}
   ny=${ny_arr[$i]}
   nz=${nz_arr[$i]}
+  cbx=${cbx_arr[$i]}
+  cby=${cby_arr[$i]}
+  cbz=${cbz_arr[$i]}
   echo "grid nx=${nx}, ny=${ny}, nz=${nz}, OMP_NUM_THREADS=${OMP_NUM_THREADS}"
   grid_str="${nx}_${ny}_${nz}"
 
   ###*********** SB ************###
   echo "Running SB"
   echo "Running 1st order"
-  cbx=64; cby=22; cbz=9999;
   srun --nodes=1 --cpus-per-task=$OMP_NUM_THREADS --hint=nomultithread --threads-per-core=1 \
   ./bin/modeling --verbose --n1 $nx  --n2 $ny --n3 $nz --iter $NT_SB_1st \
   --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot --src_depth $src_depth --order 1 --fmax $fmax \
-  --dx $dx --cbx $cbx --cby $cby --cbz $cbz  >> $logs_path/log-SB_1st-abc_$grid_str.log
+  --dx $dx --cbx $cbx --cby $cby --cbz $cbz  >> $logs_path/test1_fwd.log
 
   ###*********** TB ************##
   echo "Running TB"
@@ -127,5 +141,5 @@ for i in $(seq 0 $len); do
   ./bin/modeling --verbose --n1 $nx --n2 $ny --n3 $nz --iter $NT_TB_1st --tb_thread_group_size $tgs \
   --tb_nb_thread_groups $(expr $OMP_NUM_THREADS / $tgs) --tb_th_x $th_x --tb_th_y $th_y --tb_th_z $th_z \
   --tb_t_dim $t_dim --tb_num_wf $num_wf --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot -c \
-  --src_depth $src_depth --order 1 --fmax $fmax --dx $dx >> $logs_path/log-TB_1st-abc_$grid_str.log;
+  --src_depth $src_depth --order 1 --fmax $fmax --dx $dx >> $logs_path/test1_fwd.log;
 done
