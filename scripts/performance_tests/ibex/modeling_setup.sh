a0803043d14f030a411b35e62a1c0a4941daaac0
Pavel Plotnitskii <pavelplotnitskii@Pavels-MacBook-Air.local>
2026-04-09 17:13:55 +0300

    make push

--- scripts/performance_tests/ibex/modeling_setup.sh
+++ scripts/performance_tests/ibex/modeling_setup.sh
@@ -1,160 +1,85 @@
 #!/bin/bash
-###********* SLURM CONFIGURATION **********###
-#SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=pavel.plotnitskii@kaust.edu.sa
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --threads-per-core=1
-#SBATCH --time=24:00:00
-#SBATCH --partition=workq  #
-#SBATCH --job-name=test1_modeling
-#SBATCH --output=logs/test1_modeling.%J.out
-#SBATCH --error=logs/test1_modeling.%J.err
-#SBATCH --cpus-per-task=128
-#SBATCH --hint=nomultithread    # don't use hyperthreading
+###********** OPENMP PARAMETERS ***********###
+export OMP_NUM_THREADS=128
+export OMP_PROC_BIND=true
+export OMP_PLACES=threads
+export OMP_NESTED='True'
+export granularity=fine
+export KMP_AFFINITY=compact
+export KMP_HW_SUBSET=1t
 
-###******** COMMENT *********###
-# In this test we check performance with best SB,TB parameters for AMD MilanX.
-# be aware that cache blocking should be different for 1st order and second order
-# 1)please change SLURM CONFIGURATION accordingly
-#  partition,
-# number of cpus-per-task should be equal to number of available cores.
-# 2)please change OPENMP PARAMETERS accordingly
-#OMP_NUM_THREADS should be equal to number of cpus
-# 3)please change MODULES accordingly
-#we need to load cmake,icpc modules from somewhere
+# Set compiler flags
+export CFLAGS="-march=core-avx2 -mtune=core-avx2 -qopenmp -O3"
+export CXXFLAGS="-march=core-avx2 -mtune=core-avx2 -qopenmp -O3"
+export FFLAGS="-march=core-avx2 -mtune=core-avx2 -qopenmp -O3"
 
-###******** HORODATED LOG WRITING *********###
-echo $hostname
+#export CFLAGS="-xHost -march=core-avx2 -mtune=core-avx2 -qopenmp -O3"
+#export CXXFLAGS="-xHost -march=core-avx2 -mtune=core-avx2 -qopenmp -O3"
+#export FFLAGS="-xHost -march=core-avx2 -mtune=core-avx2 -qopenmp -O3"
+
 lscpu
 
-####********** OPENMP PARAMETERS ***********###
-#export OMP_NUM_THREADS=192
-#export OMP_PROC_BIND=true
-#export OMP_PLACES=threads
-#export OMP_NESTED='True'
-#export granularity=fine
-#export KMP_AFFINITY=compact
-#export KMP_HW_SUBSET=1t
-###********** Set compiler flags *********###
-#export CFLAGS="-march=core-avx2 -mtune=core-avx2 -qopenmp -O3"
-#export CXXFLAGS="-march=core-avx2 -mtune=core-avx2 -qopenmp -O3"
-#export FFLAGS="-march=core-avx2 -mtune=core-avx2 -qopenmp -O3"
-
-######################################################
-export OMP_NUM_THREADS=192;
-export OMP_PLACES=cores;
-export OMP_PROC_BIND=close;
-export OMP_STACKSIZE=64M;
-#export CFLAGS="-march=znver4 -dynamic -m64 -Ofast -ffast-math -fopenmp -O3"
-#export CXXFLAGS="-march=znver4 -dynamic -m64 -Ofast -ffast-math -fopenmp -O3"
-#export FFLAGS="-march=znver4 -dynamic -m64 -Ofast -ffast-math -fopenmp -O3"
-
-export CFLAGS="-march=znver4 -Ofast -fopenmp -fvectorize -floop-nest-optimize -floop-interchange -fno-math-errno -flto -mamdlibm"
-export CXXFLAGS="-march=znver4 -Ofast -fopenmp -fvectorize -floop-nest-optimize -floop-interchange -fno-math-errno -flto -mamdlibm"
-export FFLAGS="-march=znver4 -Ofast -fopenmp -fvectorize -floop-nest-optimize -floop-interchange -fno-math-errno -flto -mamdlibm"
-
 ###********** MODULES *********###
-########module load intel/2024.2.1
-#module load intel-oneapi/2023.1.0
-module load aocc
+# Load required modules
+#module load intel-oneapi-compilers/2021.4.0/gcc-7.5.0-sqbobre
+#module load icc/2020.2.254
+module load intel-oneapi-compilers/2022.2.1/gcc-11.3.0-k2f52ij
+#source ~/.bashrc
 module load cmake
 
+exit 0
+
+###***********************###
+# Make sure the Intel environment is sourced properly
+# Set the correct compiler paths (ensure the Intel compiler is used)
+#export CC=/opt/intel/oneapi/compiler/2023.2.4/linux/bin/icx
+#export CXX=/opt/intel/oneapi/compiler/2023.2.4/linux/bin/icpx
+###***********************###
+
 ##### Shot information #####
-export shot=32896;  # position of the source in x,y coordinates.check ./data/acquisition.txt
+export shot=32896;  # position of the source in x,y coordinates. Check ./data/acquisition.txt
 export src_depth=256;
 export fmax=8;
-export dh=10;
-export dt=0.001;
-
-###********** Default SB, TB parameters *********###
-cbx_arr=(8  16 22)
-cby_arr=(6  6  46)
-cbz_arr=(9999  9999  9999)
-
-### My recent parameter search
-#th_x_arr_1st=(16 16 16)
-#th_y_arr_1st=(2 2 2)
-#th_z_arr_1st=(1 1 1)
-#tdim_arr_1st=(7 7 7)
-#num_wf_arr_1st=(192 192 192)
-
-# PASC paper results
-th_x_arr_1st=(16 4 4)
-th_y_arr_1st=(2 2 2)
-th_z_arr_1st=(1 1 1)
-tdim_arr_1st=(7 3 7)
-num_wf_arr_1st=(192 20 4)
-
-### PASC-based results, my guess
-#th_x_arr_1st=(8 2 2)
-#th_y_arr_1st=(1 2 2)
-#th_z_arr_1st=(1 1 1)
-#tdim_arr_1st=(3 7 7)
-#num_wf_arr_1st=(24 20 20)
+export dx=10;
 
 ###*********** Experiment setup ************###
-nx_arr=(  512  1024  2048  )
-ny_arr=(  512  1024  2048  )
-nz_arr=(  512  512   512   )
-export NT_TB_1st=505
-export NT_SB_1st=505
-#export NT_TB_1st=4001
-#export NT_SB_1st=4001
+nx=512; ny=512; nz=512;
+export NT_TB_2nd=530  # @pavel in TB source injection starts from second time sample (Nothing happens for one dt). This is code feature.
+export NT_SB_2nd=529  # @pavel in SB the nt should one time less than in corresponding TB.
+export NT_TB_1st=537  # @pavel in TB source injection starts from second time sample (Nothing happens for one dt). This is code feature.
+export NT_SB_1st=536  # @pavel in SB the nt should one time less than in corresponding TB.
+export NT_SB_1st=100
+pwd
+
+##### Logs directory #####
+export logs_path="./logs/reproduce_sb"
+mkdir "$logs_path"
+
+##### File to modify #####
+export file="./include/stencil/wave.h"
+
+##### Cache blocking to try #####
+x=10; y=22;
+
+##### Run tests #####
+grid_str="${nx}_${ny}_${nz}_${x}_${y}"
+echo $grid_str;
+
+##### Change cache blocking values #####
+sed -i "s/#define BLOCKX [0-9]\+/#define BLOCKX $x/" "$file"
+sed -i "s/#define BLOCKY [0-9]\+/#define BLOCKY $y/" "$file"
 
 ##### COMPILATION #####
-mv -f ./CMakeCache.txt ./CMakeCache-old.txt    #Last CMakeCache.txt is saved
+mv -f ./CMakeCache.txt ./CMakeCache-old.txt  # Save the last CMakeCache.txt
+#cmake .
 CC=icc CXX=icpc cmake .
 make clean
 make VERBOSE=1
 make install
 
-##### Logs directory #####
-mkdir ./logs
-export logs_path=./logs/test1_modeling
-export logs_filename="test1_forward_pasc_new_attempt3_aocc.log"
-######rm -rf $logs_path
-mkdir $logs_path
-
-##### Run tests #####
-len=${#nx_arr[@]}
-for i in $(seq 2 $len); do
-#for i in $(seq 1 2); do
-  echo $i
-  nx=${nx_arr[$i]}
-  ny=${ny_arr[$i]}
-  nz=${nz_arr[$i]}
-  cbx=${cbx_arr[$i]}
-  cby=${cby_arr[$i]}
-  cbz=${cbz_arr[$i]}
-  echo "grid nx=${nx}, ny=${ny}, nz=${nz}, OMP_NUM_THREADS=${OMP_NUM_THREADS}"
-  grid_str="${nx}_${ny}_${nz}"
-  echo "cbx=${cbx}, cby=${cby}, cbz=${cbz}"
-
-  ###*********** SB ************###
-#  echo "Running SB"
-#  echo "Running 1st order"
-#  srun --nodes=1 --cpus-per-task=$OMP_NUM_THREADS --hint=nomultithread --threads-per-core=1 \
-#  ./bin/modeling --verbose --n1 $nx  --n2 $ny --n3 $nz --iter $NT_SB_1st \
-#  --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot --src_depth $src_depth --order 1 --fmax $fmax \
-#  --dx $dh --dy $dh --dz $dh --dt $dt --rec_sismos 0 \
-#  --cbx $cbx --cby $cby --cbz $cbz >> $logs_path/$logs_filename;
-
-  ###*********** TB ************##
-  echo "Running TB"
-  echo "Running 1st order"
-  th_x=${th_x_arr_1st[$i]}
-  th_y=${th_y_arr_1st[$i]}
-  th_z=${th_z_arr_1st[$i]}
-  t_dim=${tdim_arr_1st[$i]}
-  num_wf=${num_wf_arr_1st[$i]}
-  tgs=$((th_x * th_y*th_z))
-  echo "num_th=${OMP_NUM_THREADS},th_x=${th_x},th_y=${th_y},th_z=${th_z},num_wf=${num_wf},t_dim=${t_dim},tgs=${tgs}"
-
-#  srun --nodes=1 --cpus-per-task=$OMP_NUM_THREADS --hint=nomultithread --threads-per-core=1 --unbuffered numactl --interleave=all \
-  srun --nodes=1 --cpus-per-task=$OMP_NUM_THREADS --hint=nomultithread --threads-per-core=1 numactl --interleave=all \
-  ./bin/modeling --verbose --n1 $nx --n2 $ny --n3 $nz --iter $NT_TB_1st --tb_thread_group_size $tgs \
-  --tb_nb_thread_groups $(expr $OMP_NUM_THREADS / $tgs) --tb_th_x $th_x --tb_th_y $th_y --tb_th_z $th_z \
-  --tb_t_dim $t_dim --tb_num_wf $num_wf --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot -c \
-  --src_depth $src_depth --order 1 --fmax $fmax --dx $dx --rec_sismos 0 >> $logs_path/$logs_filename;
-done
+#####################
+echo "Running SB"
+echo "Running 1st order"
+./bin/modeling --verbose --n1 $nx --n2 $ny --n3 $nz --iter $NT_SB_1st \
+--mode 2 --drcv 1 --dshot 1 --first $shot --last $shot --src_depth $src_depth --order 1 --fmax $fmax \
+--dx $dx
