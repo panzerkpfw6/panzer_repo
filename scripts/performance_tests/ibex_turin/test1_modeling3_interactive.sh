0596e474f557d00a5483f9da3962f5f03615f2b7
Pavel Plotnitskii <x_plotnips@cn514-10-l.ibex.kaust.edu.sa>
2026-05-26 19:48:37 +0300

    make push

--- scripts/performance_tests/ibex_turin/test1_modeling3_interactive.sh
+++ scripts/performance_tests/ibex_turin/test1_modeling3_interactive.sh
@@ -5,10 +5,10 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --threads-per-core=1
-#SBATCH --cpus-per-task=128
+#SBATCH --cpus-per-task=192
 #SBATCH --time=3:00:00
-####SBATCH --partition=amd  #
-#SBATCH --job-name=test1_modeling
+#SBATCH --constraint=turin
+#SBATCH --job-name=test1_TUR
 #SBATCH --output=logs/test1_modeling.%J.out
 #SBATCH --error=logs/test1_modeling.%J.err
 #SBATCH --hint=nomultithread    # don't use hyperthreading
@@ -33,27 +33,18 @@ echo $hostname
 lscpu
 
 ####********** OPENMP PARAMETERS ***********###
-#export OMP_NUM_THREADS=192
-#export OMP_PROC_BIND=true
-#export OMP_PLACES=threads
-#export OMP_NESTED='True'
-#export granularity=fine
-#export KMP_AFFINITY=compact
-#export KMP_HW_SUBSET=1t
 
 ######################################################
-export OMP_NUM_THREADS=128;
-export OMP_PLACES=cores;
-export OMP_PROC_BIND=close;
-export OMP_STACKSIZE=64M;
+export OMP_NUM_THREADS=192
+export OMP_PLACES=cores
+export OMP_PROC_BIND=close
+export OMP_STACKSIZE=64M
 
-# #export CFLAGS="-march=znver4 -dynamic -m64 -Ofast -ffast-math -fopenmp -O3"
-# # export CFLAGS="-march=znver4 -Ofast -fopenmp -fvectorize -floop-nest-optimize -floop-interchange -fno-math-errno -flto -mamdlibm"
-# # export CFLAGS="-march=znver2 -m64 -Ofast -ffast-math -qopenmp -O3"
-
-export CFLAGS="-march=native -O3 -ffast-math -qopenmp -qopt-report=2"
+# export CFLAGS="-march=native -O3 -ffast-math -qopenmp -qopt-zmm-usage=high -mprefer-vector-width=512 -qopt-report=3"
+export CFLAGS="-march=native -O3 -ffast-math -qopenmp"
 export CXXFLAGS="$CFLAGS"
 export FFLAGS="$CFLAGS"
+######################################################
 
 ###********** MODULES *********###
 module purge
@@ -79,26 +70,18 @@ cbx_arr=(16  16 16)
 cby_arr=(4  4  4)
 cbz_arr=(9999  9999  9999)
 
-### My recent parameter search
-#th_x_arr_1st=(16 16 16)
-#th_y_arr_1st=(2 2 2)
-#th_z_arr_1st=(1 1 1)
-#tdim_arr_1st=(7 7 7)
-#num_wf_arr_1st=(192 192 192)
-
-# PASC paper results
-th_x_arr_1st=(8 4 4)
+th_x_arr_1st=(16 4 4)
 th_y_arr_1st=(2 2 2)
 th_z_arr_1st=(1 1 1)
-tdim_arr_1st=(7 7 7)
-num_wf_arr_1st=(64 20 20)
+num_wf_arr_1st=(192 20 4)
+tdim_arr_1st=(7 3 7)
 
-### PASC-based results, my guess
-#th_x_arr_1st=(8 2 2)
-#th_y_arr_1st=(1 2 2)
-#th_z_arr_1st=(1 1 1)
-#tdim_arr_1st=(3 7 7)
-#num_wf_arr_1st=(24 20 20)
+# th_x_arr_1st=(1 1 1)
+# th_y_arr_1st=(1 1 1)
+# th_z_arr_1st=(16 16 16)
+# num_wf_arr_1st=(192 192 192)
+# tdim_arr_1st=(7 7 7)
+
 
 ###*********** Experiment setup ************###
 nx_arr=(  512  1024  2048  )
@@ -120,7 +103,7 @@ make install
 mkdir ./logs
 export logs_path=./logs/test1_modeling
 export logs_filename="test1_interactive_old.log"
-export logs_filename="test1_interactive_ongoing_testing_rome.log"
+export logs_filename="test1_interactive_ongoing_testing.log"
 rm -rf $logs_path/$logs_filename;
 mkdir $logs_path
 
@@ -160,10 +143,9 @@ for i in $(seq 0 $len); do
   tgs=$((th_x * th_y*th_z))
   echo "num_th=${OMP_NUM_THREADS},th_x=${th_x},th_y=${th_y},th_z=${th_z},num_wf=${num_wf},t_dim=${t_dim},tgs=${tgs}"
 
-#  srun --nodes=1 --cpus-per-task=$OMP_NUM_THREADS --hint=nomultithread --threads-per-core=1 --unbuffered numactl --interleave=all \
-  # ./bin/modeling --verbose --n1 $nx --n2 $ny --n3 $nz --iter $NT_TB_1st --tb_thread_group_size $tgs \
-  numactl --interleave=all ./bin/modeling --verbose --n1 $nx --n2 $ny --n3 $nz --iter $NT_TB_1st --tb_thread_group_size $tgs \
+  # numactl --interleave=all ./bin/modeling --verbose --n1 $nx --n2 $ny --n3 $nz --iter $NT_TB_1st --tb_thread_group_size $tgs \
+  srun --nodes=1 --cpus-per-task=$OMP_NUM_THREADS --hint=nomultithread --threads-per-core=1 --unbuffered numactl --interleave=all \
   --tb_nb_thread_groups $(expr $OMP_NUM_THREADS / $tgs) --tb_th_x $th_x --tb_th_y $th_y --tb_th_z $th_z \
   --tb_t_dim $t_dim --tb_num_wf $num_wf --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot -c \
-  --src_depth $src_depth --order 1 --fmax $fmax --dx $dh --dy $dh --dz $dh --dt $dt --rec_sismos 0  >> $logs_path/$logs_filename;
+  --src_depth $src_depth --order 1 --fmax $fmax --dx $dh --dy $dh --dz $dh >> $logs_path/$logs_filename;
 done
