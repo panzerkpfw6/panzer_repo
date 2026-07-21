4c694b56dd40c981154205461840addbaa598d52
Olena Samonina <samonio@gpu502-01.ibex.kaust.edu.sa>
2026-07-22 00:25:10 +0300

    make push

--- scripts/performance_tests/ibex_gpu/test0.sh
+++ scripts/performance_tests/ibex_gpu/test0.sh
@@ -52,7 +52,7 @@ cmake \
   -DCMAKE_C_COMPILER="$(which icx)" \
   -DCMAKE_CXX_COMPILER="$(which icpx)" \
   -DCMAKE_CUDA_COMPILER="$(which nvcc)" \
-  -DCMAKE_CUDA_ARCHITECTURES=61,70 \
+  -DCMAKE_CUDA_ARCHITECTURES=61;70 \
   -DUSE_CUDA=ON \
   .
 make clean
@@ -114,18 +114,18 @@ export cbz=9999
 # --tb_t_dim $t --tb_num_wf $w --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot -c --src_depth $src_depth --order 1 --fmax 8 \
 # --method "TB";
 ###############################
-echo "test_SB_on_GPU"
-nx=128;ny=256;nz=512;
-nt=10;  dt=0.001;
-export shot=16447;  # position of the source in x,y coordinates.check ./data/acquisition.txt
-export src_depth=256;
-export fmax=8;
+# echo "test_SB_on_GPU"
+# nx=128;ny=256;nz=512;
+# nt=10;  dt=0.001;
+# export shot=16447;  # position of the source in x,y coordinates.check ./data/acquisition.txt
+# export src_depth=256;
+# export fmax=8;
 
-./bin/modeling --verbose --n1 $nx  --n2 $ny --n3 $nz --iter $NT_SB_1st \
---mode 2 --drcv 1 --dshot 1 --first $shot --last $shot --src_depth $src_depth --order 1 --fmax $fmax \
---dx $dh --dy $dh --dz $dh --dt $dt --rec_sismos 0 \
---cbx $cbx --cby $cby --cbz $cbz --cpu 0 --method "SB" # >> $logs_path/$logs_filename;
-exit 1
+# ./bin/modeling --verbose --n1 $nx  --n2 $ny --n3 $nz --iter $NT_SB_1st \
+# --mode 2 --drcv 1 --dshot 1 --first $shot --last $shot --src_depth $src_depth --order 1 --fmax $fmax \
+# --dx $dh --dy $dh --dz $dh --dt $dt --rec_sismos 0 \
+# --cbx $cbx --cby $cby --cbz $cbz --cpu 0 --method "SB" # >> $logs_path/$logs_filename;
+# exit 1
 
 ###############################
 echo "test_TB_on_GPU"
